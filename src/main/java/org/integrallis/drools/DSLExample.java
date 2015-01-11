9943287293e4372e9b1ff5d5942e119f2c788deb
Brian Sam-Bodden <bsbodden@integrallis.com>
2015-01-11 08:36:32 -0700

    Updated for Drools 6.1.0

--- src/main/java/org/integrallis/drools/DSLExample.java
+++ src/main/java/org/integrallis/drools/DSLExample.java
@@ -1,43 +1,18 @@
 package org.integrallis.drools;
 
-import org.drools.KnowledgeBase;
-import org.drools.KnowledgeBaseFactory;
-import org.drools.builder.KnowledgeBuilder;
-import org.drools.builder.KnowledgeBuilderError;
-import org.drools.builder.KnowledgeBuilderErrors;
-import org.drools.builder.KnowledgeBuilderFactory;
-import org.drools.builder.ResourceType;
-import org.drools.io.ResourceFactory;
-import org.drools.logger.KnowledgeRuntimeLogger;
-import org.drools.logger.KnowledgeRuntimeLoggerFactory;
-import org.drools.runtime.StatefulKnowledgeSession;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 
 public class DSLExample {
 
 	public static final void main(String[] args) {
-		try {
-			// 1 - load the rules in to a knowledge builder
-			KnowledgeBuilder knowledgeBuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-			
-			knowledgeBuilder.add(ResourceFactory.newClassPathResource("say_something.dsl"), ResourceType.DSL);
-			knowledgeBuilder.add(ResourceFactory.newClassPathResource("rockys_rules.dslr"), ResourceType.DSLR);			
-			
-			KnowledgeBuilderErrors errors = knowledgeBuilder.getErrors();
-			if (errors.size() > 0) {
-				for (KnowledgeBuilderError error: errors) {
-					System.err.println(error);
-				}
-				throw new IllegalArgumentException("Could not parse knowledge.");
-			}
-			// 2 - create a knowledge base using a knowledge builder
-			KnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();
-			knowledgeBase.addKnowledgePackages(knowledgeBuilder.getKnowledgePackages());
-			
-			// 3 - create a stateful knowledge session
-			StatefulKnowledgeSession knowledgeSession = knowledgeBase.newStatefulKnowledgeSession();
-			
-			// create a logger for the knowledge session
-			KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(knowledgeSession, "test");
+	    KieSession knowledgeSession = null;
+	    try {
+	    		// load up the knowledge base
+	    		KieServices ks = KieServices.Factory.get();
+	        KieContainer kContainer = ks.getKieClasspathContainer();
+	        knowledgeSession = kContainer.newKieSession("ksession-rules");
 			
 			// 4 - create and assert some facts
 			Person rocky = new Person("Rocky Balboa", "Philadelphia", 35);
@@ -46,10 +21,10 @@ public class DSLExample {
 		
 			// 5 - fire the rules
 			knowledgeSession.fireAllRules();
-			
-			logger.close();
 		} catch (Throwable t) {
 			t.printStackTrace();
-		}
+		} finally {
+			knowledgeSession.dispose();
+        }
 	}
 }
--- src/test/java/org/integrallis/drools/DSLTest.java
+++ src/test/java/org/integrallis/drools/DSLTest.java
@@ -2,34 +2,36 @@ package org.integrallis.drools;
 
 import static org.junit.Assert.assertEquals;
 
-import org.drools.runtime.rule.QueryResults;
-import org.drools.runtime.rule.QueryResultsRow;
+import org.kie.api.runtime.rule.QueryResults;
+import org.kie.api.runtime.rule.QueryResultsRow;
 import org.integrallis.drools.junit.BaseDroolsTestCase;
 import org.junit.Test;
+
+
 
 public class DSLTest extends BaseDroolsTestCase {
 	
     public DSLTest() {
-		super("rockys_rules.dslr", "say_something.dsl");
+    	  super("ksession-rules");
 	}
 
 	@Test
     public void testPersonMeantTucsonRule() {
-    	Person bob = new Person("Bob", "Tooson", 35);
-    	Person jim = new Person("Jim", "Tuzzon", 25);
-    	Person charlie = new Person("Charlie", "Tucson", 44);
-    	Person fred = new Person("Fred", "too zone", 23);
-    	Person julia = new Person("Julia", "two so on", 45);
-    	Person colin = new Person("Colin", "Pasadena", 19);
-    	
-    	knowledgeSession.insert(bob);
-    	knowledgeSession.insert(jim);
-    	knowledgeSession.insert(charlie);
-    	knowledgeSession.insert(fred);
-    	knowledgeSession.insert(julia);
-    	knowledgeSession.insert(colin);
-    	
-    	knowledgeSession.fireAllRules();
+	    	Person bob = new Person("Bob", "Tooson", 35);
+	    	Person jim = new Person("Jim", "Tuzzon", 25);
+	    	Person charlie = new Person("Charlie", "Tucson", 44);
+	    	Person fred = new Person("Fred", "too zone", 23);
+	    	Person julia = new Person("Julia", "two so on", 45);
+	    	Person colin = new Person("Colin", "Pasadena", 19);
+	    	
+	    	knowledgeSession.insert(bob);
+	    	knowledgeSession.insert(jim);
+	    	knowledgeSession.insert(charlie);
+	    	knowledgeSession.insert(fred);
+	    	knowledgeSession.insert(julia);
+	    	knowledgeSession.insert(colin);
+	    	
+	    	knowledgeSession.fireAllRules();
     	
 		QueryResults results = knowledgeSession.getQueryResults( "Get all Messages" );
 		assertEquals(4, results.size());
